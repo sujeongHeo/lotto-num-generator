53707bfdfc91c3c4a058eefb5e576a3c9f551c98
sujeongHeo <gjtnwjd40@gmail.com>
2021-04-27 01:20:35 +0900

    자동 생성 버튼 기능 추가

--- MainActivity.kt
+++ MainActivity.kt
@@ -60,6 +60,13 @@ class MainActivity : AppCompatActivity() {
             val list = getRandomNumber()
 
             didRun = true
+
+            list.forEachIndexed { index, number ->
+                val textView = numberTextViewList[index]
+
+                textView.text = number.toString()
+                textView.isVisible = true
+            }
             Log.d("MainActivity", list.toString())
         }
     }
@@ -99,12 +106,15 @@ class MainActivity : AppCompatActivity() {
         val numberList = mutableListOf<Int>()
             .apply {
                 for (i in 1..45) {
+                    if (pickNumberSet.contains(i)) {
+                        continue
+                    }
                     this.add(i)
                 }
             }
         numberList.shuffle()
 
-        val newList = numberList.subList(0, 6)
+        val newList = pickNumberSet.toList() + numberList.subList(0, 6 - pickNumberSet.size)
         return newList.sorted()
     }
 }
